bf1e28037c7361408ddb48d093f1673f064d461e
mellownightpirate <a@hasan.co>
2020-06-28 14:27:47 +0100

    fixed mongoURI typo

--- banking-app/config/keys.js
+++ banking-app/config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://mellownightpirate:<banking-app-1568>@banking-app-cluster0-bvbzp.mongodb.net/<mellownightpirate>?retryWrites=true&w=majority",
+    "mongodb+srv://mellownightpirate:banking-app-1568@banking-app-cluster0-bvbzp.mongodb.net/<mellownightpirate>?retryWrites=true&w=majority",
 };
