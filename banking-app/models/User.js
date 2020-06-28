25a34998a23f4a829ebbb7432d185b2e6ac446aa
mellownightpirate <a@hasan.co>
2020-06-28 14:37:02 +0100

    setting up our database schema

---
+++ banking-app/models/User.js
@@ -0,0 +1,25 @@
+// pull in our required dependencies
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Schema
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = User = mongoose.model("users", UserSchema)
