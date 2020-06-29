360673e2ee34a031808d32e6bdbcefd577516777
mellownightpirate <a@hasan.co>
2020-06-29 12:32:03 +0100

    create register endpoint

--- banking-app/routes/api/users.js
+++ banking-app/routes/api/users.js
@@ -1,7 +1,7 @@
 // pull in dependencies and input validations and user models
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcruptjs");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config / keys");
 
@@ -11,3 +11,39 @@ const validateLoginInput = require("../../validation/login");
 
 // Load User model
 const User = require("../../models/User");
+// POST api/users/register
+router.post("/register", (req, res) => {
+  // Form validation
+  // Pull the errors and isValid variables from our validateRegisterInput(req.body) function
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+// If valid input, use MongoDB's User.findOne() to see if the user already exists
+    User.findOne({ email: req.body.email }).then((user) => {
+    // If user is a new user, fill in the fields (name, email, password) with data sent in the body of the request
+    if (user) {
+      return res.status(400).json({ email: "Email already exists" });
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      });
+
+      // Hash password before saving in database
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then((user) => res.json(user))
+            .catch((err) => console.log(err));
+        });
+      });
+    }
+  });
+});
