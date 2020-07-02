0d4d6ce8dc78766f040fd00df1dc3856ae17cf3f
mellownightpirate <a@hasan.co>
2020-07-02 16:42:05 +0100

    create directory for auth components and create Login. js and Register.js

---
+++ banking-app/client/src/components/auth/Register.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Register extends Component {
+    constructor() {
+        super();
+        this.state = {
+            name: "",
+            email: "",
+            password: "",
+            password2: "",
+            errors: {}
+        };
+    }
+    
+ }
