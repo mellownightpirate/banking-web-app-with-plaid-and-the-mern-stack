d3ce77cc7928922dc076fd58c9cb16bce67c2c98
mellownightpirate <a@hasan.co>
2020-07-03 11:00:36 +0100

    setting up React Router in App.js

--- banking-app/client/src/App.js
+++ banking-app/client/src/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from "react";
-import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
+// pull in components
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
         <Navbar />
-        <Landing />
-      </div>
+          <Landing />
+          {/* define routing paths */}
+          <Route exact path="/" component={Landing} />
+          {/* E.g. render the register component at localhost:3000/register */}
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </div>
+        </Router>
     );
   }
 }
