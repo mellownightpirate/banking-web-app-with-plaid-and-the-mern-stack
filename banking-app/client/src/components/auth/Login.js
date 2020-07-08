f97ab51208fe074972667ab96789b602997fc8c9
mellownightpirate <a@hasan.co>
2020-07-08 17:50:56 +0100

    add lifecycle methods to Register and Login so that logged in users navigate to either of these they are redirected to the dashboard

--- banking-app/client/src/components/auth/Login.js
+++ banking-app/client/src/components/auth/Login.js
@@ -14,6 +14,13 @@ class Login extends Component {
     };
   }
 
+  componentDidMount() {
+    // If logged in and user navigates to Login page, should redirect them to dashboard
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+  }
+
   componentWillReceiveProps(nextProps) {
     if (mextProps.auth.isAuthenticated) {
       this.props.history.push("/dashboard"); // push user to dashboard when they login
