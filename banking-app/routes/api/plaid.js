fe4783b1aba5810ae2e8efe591fe279bfcbc11dd
mellownightpirate <a@hasan.co>
2020-07-08 22:29:59 +0100

    get all bank accounts that a specific user has linked so we can display them upon logging in

--- banking-app/routes/api/plaid.js
+++ banking-app/routes/api/plaid.js
@@ -4,12 +4,15 @@ const router = express.Router();
 const passport = require("passport");
 const moment = require("moment");
 const mongoose = require("mongoose");
+
 // Load Account and User models
 const Account = require("../../models/Account");
 const User = require("../../models/User");
+
 const PLAID_CLIENT_ID = "5f05e314f330e200121f05eb";
 const PLAID_SECRET = "51d2c7c6c762af714152ec7a89dc28";
 const PLAID_PUBLIC_KEY = "d216519a084a5b106b08f58d01fcba";
+
 const client = new plaid.Client(
   PLAID_CLIENT_ID,
   PLAID_SECRET,
@@ -17,8 +20,23 @@ const client = new plaid.Client(
   plaid.environments.sandbox,
   { version: "2018-05-22" }
 );
+
 var PUBLIC_TOKEN = null;
 var ACCESS_TOKEN = null;
 var ITEM_ID = null;
+
 // Routes will go here
+// GET  api/plaid/accounts
+// Get all accounts linked with plaid for a specific user
+// Private
+router.get(
+  "/accounts",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Account.find({ userId: req.user.id })
+      .then(accounts => res.json(accounts))
+      .catch(err => console.log(err));
+  }
+);
+
 module.exports = router;
