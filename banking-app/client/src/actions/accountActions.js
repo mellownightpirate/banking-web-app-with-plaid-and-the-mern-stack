82a1ce09471d67c7b51ac98c1deb93032f46e4f1
mellownightpirate <a@hasan.co>
2020-07-09 21:20:14 +0100

    deleteAccount action

--- banking-app/client/src/actions/accountActions.js
+++ banking-app/client/src/actions/accountActions.js
@@ -27,3 +27,23 @@ export const addAccount = plaidData => dispatch => {
       .catch(err => console.log(err));
 };
   
+// Delete account
+export const deleteAccount = plaidData => dispatch => {
+    if (window.confirm("Are you sure you want to remove this account?")) {
+      const id = plaidData.id;
+      const newAccounts = plaidData.accounts.filter(
+        account => account._id !== id
+      );
+      axios
+        .delete(`/api/plaid/accounts/${id}`)
+        .then(res =>
+          dispatch({
+            type: DELETE_ACCOUNT,
+            payload: id
+          })
+        )
+        .then(newAccounts ? dispatch(getTransactions(newAccounts)) : null)
+        .catch(err => console.log(err));
+    }
+};
+  
