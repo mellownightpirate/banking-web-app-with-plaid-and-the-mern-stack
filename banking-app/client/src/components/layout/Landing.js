6f170dfb81612c779ce331915eb94c8fadbd56fc
mellownightpirate <a@hasan.co>
2020-07-09 22:18:35 +0100

    dashboard and landing

--- banking-app/client/src/components/layout/Landing.js
+++ banking-app/client/src/components/layout/Landing.js
@@ -1,42 +1,54 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import credit_card from "../../img/credit_card.png";
 class Landing extends Component {
-  render() {
+  componentDidMount() {
+    // If logged in, should redirect them to dashboard
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+  }
+render() {
     return (
-      <div style={{ height: "50vh" }} className="container valign-wrapper">
+      <div style={{ height: "75vh" }} className="container valign-wrapper">
         <div className="row">
           <div className="col s12 center-align">
-            <h4>
-              <b>Connect</b> your bank details to {" "}
-              <span style={{ fontFamily: "monospace" }}>OpenBank</span> safely and
+            <img
+              src={credit_card}
+              style={{ width: "350px" }}
+              className="responsive-img credit-card"
+              alt="Undraw"
+            />
+            <h4 className="flow-text">
+            <b>Connect</b> your bank details to {" "}
+            <span style={{ fontFamily: "monospace" }}>OpenBank</span> safely and
               securely
             </h4>
-            <p className="flow-text grey-text text-darken-1">
-              The (easiest) way for users to connect their bank accounts to an app
-            </p>
             <br />
-            <div className="col push-s4">
+            <div className="col s6">
               <Link
                 to="/register"
                 style={{
                   width: "140px",
                   borderRadius: "3px",
-                  letterSpacing: "1.5px",
+                  letterSpacing: "1.5px"
                 }}
                 className="btn btn-large waves-effect waves-light hoverable blue accent-3"
               >
                 Register
               </Link>
             </div>
-            <div className="col push-s4">
+            <div className="col s6">
               <Link
                 to="/login"
                 style={{
                   width: "140px",
                   borderRadius: "3px",
-                  letterSpacing: "1.5px",
+                  letterSpacing: "1.5px"
                 }}
-                className="btn btn-large waves-effect waves-light hoverable white black-text"
+                className="btn btn-large btn-flat waves-effect white black-text"
               >
                 Log In
               </Link>
@@ -47,4 +59,10 @@ class Landing extends Component {
     );
   }
 }
-export default Landing;
+Landing.propTypes = {
+  auth: PropTypes.object.isRequired
+};
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+export default connect(mapStateToProps)(Landing);
